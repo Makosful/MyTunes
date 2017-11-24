554ecf8fb11ddc4baee58244abc6ace75b44ce9e
dankphetamine <asge0907@easv365.dk>
2017-11-25 00:41:09 +0100

    Added tabbed layout (bottom left)
    Includes Non-started Equalizer & a finished Filechooser with limitations to .mp3 files.

--- src/mytunes/gui/controller/MainWindowController.java
+++ src/mytunes/gui/controller/MainWindowController.java
@@ -1,28 +1,28 @@
 package mytunes.gui.controller;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXListView;
 import com.jfoenix.controls.JFXSlider;
+import com.jfoenix.controls.JFXTabPane;
 import java.io.File;
 import java.net.URL;
-import java.util.Observable;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.beans.InvalidationListener;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.control.Tab;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
-
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 /**
  *
  * @author Axl
@@ -31,16 +31,29 @@ public class MainWindowController implements Initializable
 {
     // <editor-fold defaultstate="collapsed" desc=" FXML & Variables">
     @FXML
+    private JFXButton btnStop;
+    @FXML
+    private JFXButton btnLoadMP3;
+    @FXML
+    private JFXButton btnLoadMP3Multi;
+    @FXML
+    private JFXListView<String> listLoadedMP3;
+    @FXML
+    private Tab tabLoadMP3;
+    @FXML
+    private Tab tabEqualizer;
+    @FXML
+    private Pane bottomPane;
+    @FXML
+    private JFXTabPane tabPane;
+    @FXML
     private JFXButton btnPlayPause;
     @FXML
     private JFXSlider volumeSlider;
     @FXML
-    private JFXButton btnStop;
-    @FXML
     private Label lblTimer;
     @FXML
     private Slider progressSlider;
-    @FXML
     private Pane mediaPane;
     @FXML
     private MediaView mediaView;
@@ -50,6 +63,7 @@ public class MainWindowController implements Initializable
     private MediaPlayer mPlayer;
     private boolean isPlaying;
     // </editor-fold>
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) 
@@ -63,11 +77,11 @@ public class MainWindowController implements Initializable
 
     private void mediaPlayerSetup() {
         String musicFile = "src/mytunes/media/testmusic.mp3";
-        
         Media song = new Media(new File(musicFile).toURI().toString());
+        
         mPlayer = new MediaPlayer(song);
         mediaView = new MediaView(mPlayer);
-        mediaPane.getChildren().add(mediaView);
+        //mediaPane.getChildren().add(mediaView);
     }
 
     @FXML
@@ -113,40 +127,37 @@ public class MainWindowController implements Initializable
         // Creating a list starting from 0+1 (convert index to number in list)    
         System.out.println("the line is #: " + (playbackIndex + 1));
         
-        if(playbackIndex == 0)
-        {
-            System.out.println("50%");
-            mPlayer.setRate(0.5);
-        }
-        else if(playbackIndex == 1)
-        {
-            System.out.println("75%");
-            mPlayer.setRate(0.75);
-        }
-        else if(playbackIndex == 2)
-        {
-            System.out.println("100%");
-            mPlayer.setRate(1.0);
-        }
-        else if(playbackIndex == 3)
-        {
-            System.out.println("125%");
-            mPlayer.setRate(1.25);
-        }
-        else if(playbackIndex == 4)
-        {
-            System.out.println("150%");
-            mPlayer.setRate(1.5);
-        }
-        else if(playbackIndex == 5)
-        {
-            System.out.println("175%");
-            mPlayer.setRate(1.75);
-        }
-        else if(playbackIndex == 6)
-        {
-            System.out.println("200%");
-            mPlayer.setRate(2.0);
+        switch (playbackIndex) {
+            case 0:
+                System.out.println("50%");
+                mPlayer.setRate(0.5);
+                break;
+            case 1:
+                System.out.println("75%");
+                mPlayer.setRate(0.75);
+                break;
+            case 2:
+                System.out.println("100%");
+                mPlayer.setRate(1.0);
+                break;
+            case 3:
+                System.out.println("125%");
+                mPlayer.setRate(1.25);
+                break;
+            case 4:
+                System.out.println("150%");
+                mPlayer.setRate(1.5);
+                break;
+            case 5:
+                System.out.println("175%");
+                mPlayer.setRate(1.75);
+                break;
+            case 6:
+                System.out.println("200%");
+                mPlayer.setRate(2.0);
+                break;
+            default:
+                break;
         }
     }
     
@@ -169,6 +180,42 @@ public class MainWindowController implements Initializable
     private void progressDrag(MouseEvent event) 
     {
         //
+    }
+
+    @FXML
+    private void LoadMP3Single(ActionEvent event) 
+    {
+        FileChooser fc = new FileChooser();
+        File chosenFile = fc.showOpenDialog(null);
+        fc.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("MP3 Files", "*.mp3"));
+        if(chosenFile != null)
+        {
+            listLoadedMP3.getItems().add(chosenFile.getAbsolutePath());
+        }
+        else
+        {
+            System.out.println("Invalid file / Not found");
+        }
+    }
+
+    @FXML
+    private void LoadMP3Multi(ActionEvent event) 
+    {
+        FileChooser fc = new FileChooser();
+        fc.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("MP3 Files", "*.mp3"));
+        
+        List<File> chosenFiles = fc.showOpenMultipleDialog(null);
+        if(chosenFiles != null)
+        {
+            for (int i = 0; i < chosenFiles.size(); i++)
+            {
+                listLoadedMP3.getItems().add(chosenFiles.get(i).getAbsolutePath());
+            }
+        }
+        else
+        {
+            System.out.println("One or more invalid file(s) / Not found");
+        }
     }
 
 }
