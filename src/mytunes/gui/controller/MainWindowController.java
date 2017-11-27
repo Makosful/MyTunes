96cf0af66be416918512912e6083089bcadb2cae
Axl <makosful@gmail.com>
2017-11-27 20:04:00 +0100

    Changed logger to sout

--- src/mytunes/gui/controller/MainWindowController.java
+++ src/mytunes/gui/controller/MainWindowController.java
@@ -10,8 +10,6 @@ import java.net.URL;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.application.Platform;
 import javafx.beans.Observable;
 import javafx.event.ActionEvent;
@@ -512,7 +510,7 @@ public class MainWindowController implements Initializable
         }
         catch (SQLException ex)
         {
-            Logger.getLogger(MainWindowController.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println(ex.getMessage());
         }
 
     }
