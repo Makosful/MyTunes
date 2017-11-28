93f24e0faba1f408d7898425b51030f1454c585a
davidmsf <davidmsf@gmail.com>
2017-11-28 18:56:36 +0100

    metadata

--- src/mytunes/bll/MetaData.java
+++ src/mytunes/bll/MetaData.java
@@ -9,9 +9,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.collections.MapChangeListener;
 import javafx.scene.media.Media;
 import mytunes.be.Music;
@@ -22,6 +19,14 @@ import mytunes.dal.SongDAO;
  * @author B
  */
 public class MetaData {
+    
+    private String artist;
+    private String title;
+    private String album;
+    private String year;
+    private String genre;
+    private String description;
+    private int iterations;
     
     SongDAO songDAO;
     
@@ -41,77 +46,93 @@ public class MetaData {
     public void MetaData(List<File> chosenFiles){
         
         
-        List<Music> tracks = new ArrayList();
-        
-        for(int i = 0; i < chosenFiles.size(); i++){
-            
+            List<Music> tracks = new ArrayList();
+
             Music track = new Music();
             
-            String fileString = chosenFiles.get(i).getAbsolutePath();
+            for(int i = 0; i < chosenFiles.size(); i++){
             
-            Media song = new Media(new File(fileString.toLowerCase()).toURI().toString());
-            
-            song.getMetadata().addListener((MapChangeListener<String, Object>) change -> {
-               
-                if (change.wasAdded()) {
-                                    
-                   
-                    if ("artist".equals(change.getKey())) 
-                    {
-                        track.setArtist(change.getValueAdded().toString());
-                      
-                    } else if ("title".equals(change.getKey())) 
-                    {
-                        track.setTitle(change.getValueAdded().toString());
+                String fileString = chosenFiles.get(i).getAbsolutePath();
+
+                Media song = new Media(new File(fileString.toLowerCase()).toURI().toString());
+                
+                 System.out.println(song.getMetadata().entrySet().toString()); 
+                
+                song.getMetadata().addListener((MapChangeListener<String, Object>) change -> {
                     
-                    } else if ("album".equals(change.getKey())) 
-                    {
-                        track.setAlbum(change.getValueAdded().toString());
-                      
-                    } else if("year".equals(change.getKey()))
-                    {
-                        track.setYear(Integer.parseInt(change.getValueAdded().toString()));
-                       
-                    } else if("genre".equals(change.getKey()))
-                    {
-                        track.setGenre(change.getValueAdded().toString());
-                       
-                    } else if("comment-N".equals(change.getKey()))
-                    {
-                        track.SetDescription(change.getValueAdded().toString());
-                       
-                    } else if("composer".equals(change.getKey()))
-                    {
-                        String whiteSpaceStart;
-                        String whiteSpaceEnd;
-                        
-                        if(track.getDescription().isEmpty()){
-                            whiteSpaceStart = "";
-                            whiteSpaceEnd = " ";
-                        }else {
-                            whiteSpaceStart = " ";
-                            whiteSpaceEnd = "";
+                    if (change.wasAdded()) {
+
+                        if ("artist".equals(change.getKey())) 
+                        {
+                            artist = change.getValueAdded().toString();
+
+                        } else if ("title".equals(change.getKey())) 
+                        {
+                            title = change.getValueAdded().toString();
+
+                        } else if ("album".equals(change.getKey())) 
+                        {
+                            album = change.getValueAdded().toString();
+
+                        } else if("year".equals(change.getKey()))
+                        {
+                            year = change.getValueAdded().toString();
+
+                        } else if("genre".equals(change.getKey()))
+                        {
+                            genre = change.getValueAdded().toString();
+
+                        } else if("comment-N".equals(change.getKey()))
+                        {
+                            description = change.getValueAdded().toString();
+
                         }
                         
-                        track.SetDescription(whiteSpaceStart+"Composers: "+change.getValueAdded().toString()+whiteSpaceEnd);
-                       
+                        System.out.println(iterations);
+                        if(artist != null && title != null && album != null && year != null){
+                            iterations++;
+                            track.setArtist(artist);
+                            track.setTitle(title);
+                            track.setAlbum(album);
+                            track.setYear(Integer.parseInt(year));
+                            track.setGenre(genre);
+                            track.SetDescription(description);
+                            
+                            tracks.add(track);
+                        
+                            finished(tracks);
+                            
+                        }
+                        
                     }
-                }
-           
-            });
+
+
+                });
             
+            }
+/**            
             if(track.getArtist().isEmpty())
             {
+
                 if(fileString.contains("."))
                 {
                    track.setArtist(chosenFiles.get(i).getName().substring(0, chosenFiles.get(i).getName().lastIndexOf(".")));
                 }
+
             }
+**/
             
-            tracks.add(track);
-       }
-        
-       songDAO.setSongs(tracks);
+           
+            
        
+  
     }
+    
+    private void finished(List<Music> tracks)
+    {
+ 
+        
+            System.out.println(tracks.get(0).getGenre());
+     }
+    
 }
