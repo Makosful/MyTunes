5ac7501e5d278748642de9d0f8a5c5b14423c4de
davidmsf <davidmsf@gmail.com>
2017-12-02 14:33:23 +0100

    meta

--- src/mytunes/bll/MetaData.java
+++ src/mytunes/bll/MetaData.java
@@ -56,7 +56,13 @@ public class MetaData {
          * @throws SAXException
          * @throws TikaException 
          */
-        public void MetaData(List<File> chosenFiles) throws FileNotFoundException, IOException, CannotReadException, TagException, ReadOnlyFileException, InvalidAudioFrameException{
+        public void MetaData(List<File> chosenFiles) throws FileNotFoundException, 
+                                                            IOException,
+                                                            CannotReadException,
+                                                            TagException,
+                                                            ReadOnlyFileException,
+                                                            InvalidAudioFrameException
+        {
 
 
             List<Music> tracks = new ArrayList();
@@ -106,13 +112,17 @@ public class MetaData {
          * @throws SAXException
          * @throws TikaException 
          */
-        private void setMetaData(File chosenFile)throws FileNotFoundException, IOException, CannotReadException, TagException, ReadOnlyFileException, InvalidAudioFrameException
+        private void setMetaData(File chosenFile)throws FileNotFoundException,
+                                                        IOException,
+                                                        CannotReadException,
+                                                        TagException,
+                                                        ReadOnlyFileException,
+                                                        InvalidAudioFrameException
         {
             
  
         
         String filetype = Files.probeContentType(chosenFile.toPath());
-        System.out.println(filetype+"!!!!!!");   
 
         if(filetype.contains("wave"))
         {
@@ -261,143 +271,4 @@ public class MetaData {
             
             return track;
         }
-
-
-
-
-
-
-
-
-
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    //OLD CODE
-    /*
-    private String artist;
-    private String title;
-    private String album;
-    private String year;
-    private String genre;
-    private String description;
-    private int iterations;
-    
-    SongDAO songDAO;
-    
-    public MetaData()
-    {
-        try {
-            this.songDAO = new SongDAO();
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-    }
-    
-    /**
-    *  Adds the mp3's metadata to music objects, then adds them to an arraylist and sends it to bllManager
-    * @param chosenFiles 
-    
-    public void MetaData(List<File> chosenFiles){
-        
-        
-            List<Music> tracks = new ArrayList();
-
-            Music track = new Music();
-            
-            for(int i = 0; i < chosenFiles.size(); i++){
-            
-                String fileString = chosenFiles.get(i).getAbsolutePath();
-
-                Media song = new Media(new File(fileString.toLowerCase()).toURI().toString());
-                
-                 System.out.println(song.getMetadata().entrySet().toString()); 
-                
-                song.getMetadata().addListener((MapChangeListener<String, Object>) change -> {
-                    
-                    if (change.wasAdded()) {
-
-                        if ("artist".equals(change.getKey())) 
-                        {
-                            artist = change.getValueAdded().toString();
-
-                        } else if ("title".equals(change.getKey())) 
-                        {
-                            title = change.getValueAdded().toString();
-
-                        } else if ("album".equals(change.getKey())) 
-                        {
-                            album = change.getValueAdded().toString();
-
-                        } else if("year".equals(change.getKey()))
-                        {
-                            year = change.getValueAdded().toString();
-
-                        } else if("genre".equals(change.getKey()))
-                        {
-                            genre = change.getValueAdded().toString();
-
-                        } else if("comment-N".equals(change.getKey()))
-                        {
-                            description = change.getValueAdded().toString();
-
-                        }
-                        
-                        System.out.println(iterations);
-                        if(artist != null && title != null && album != null && year != null){
-                            iterations++;
-                            track.setArtist(artist);
-                            track.setTitle(title);
-                            track.setAlbum(album);
-                            track.setYear(Integer.parseInt(year));
-                            track.setGenre(genre);
-                            track.SetDescription(description);
-                            
-                            tracks.add(track);
-                        
-                            finished(tracks);
-                            
-                        }
-                        
-                    }
-
-
-                });
-            
-            }
-         
-            if(track.getArtist().isEmpty())
-            {
-
-                if(fileString.contains("."))
-                {
-                   track.setArtist(chosenFiles.get(i).getName().substring(0, chosenFiles.get(i).getName().lastIndexOf(".")));
-                }
-
-            }
-
-            
-           
-            
-       
-  
-    }
-    
-    private void finished(List<Music> tracks)
-    {
- 
-        
-            System.out.println(tracks.get(0).getGenre());
-     }
- **/   
 }
